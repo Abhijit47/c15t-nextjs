c876028a0a2ff40ca81839af2c2ca7bfe605d4eb
Abhijit47 <akarmakar846@gmail.com>
2026-06-15 18:52:28 +0530

    updated c15instance config

--- src/lib/c15t.ts
+++ src/lib/c15t.ts
@@ -5,7 +5,7 @@ import { drizzleAdapter } from "@c15t/backend/db/adapters/drizzle";
 export const c15t = c15tInstance({
   appName: "c15t-nextjs",
   basePath: "/api/c15t",
-  tablePrefix: "c15t_",
+  // tablePrefix: "c15t_",
   trustedOrigins: ["http://localhost:3000", "https://c15t-nextjs.vercel.app"],
   adapter: drizzleAdapter({ db, provider: "postgresql" }),
 });
